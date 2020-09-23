58d9c8d8f63995aa2617f8424404a10db0c5947f
mametaku <mametaku.man10@gmail.com>
2020-09-23 11:07:32 +0900

    test2

---
+++ src/main/java/mametaku/automendingmachine/AutoMendingmachine.java
@@ -0,0 +1,83 @@
+package mametaku.automendingmachine;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public final class AutoMendingmachine extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("autoexpmachinerun.");
+        getServer().getPluginManager().registerEvents(this, this);
+        // config.ymlが存在しない場合はファイルに出力します。
+        saveDefaultConfig();
+        // config.ymlを読み込みます。
+        FileConfiguration config = getConfig();
+        reloadConfig();
+        if (config.getBoolean("mode")) {
+            getLogger().info("autoexpmachine not run.");
+        }
+    }
+
+
+    @EventHandler
+    public void onPlayerClicks(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+        ItemStack item = event.getItem();
+        FileConfiguration config = getConfig();
+        if (!config.getBoolean("mode")) {
+            getLogger().info("autoexpmachine not run.");
+            return;
+        }
+        if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
+            if (item != null && item.getType() == Material.STICK) {
+                player.sendMessage("経験値瓶を入れてください");
+                openGUI(player);
+            }
+        }
+    }
+
+
+    //GUIを開かせる
+    public void openGUI(Player player) {
+        Inventory inv = Bukkit.createInventory(null, 27, "EXPtank");
+        FileConfiguration config = getConfig();
+        if (!config.getBoolean("mode")) {
+            getLogger().info("autoexpmachine not run.");
+            return;
+        }
+        player.openInventory(inv);
+    }
+
+    @EventHandler
+    public void onItemClick(InventoryClickEvent event) {
+        Player p = (Player) event.getWhoClicked();
+        String q = event.getView().getTitle();
+        FileConfiguration config = getConfig();
+        if (!config.getBoolean("mode")) {
+            getLogger().info("autoexpmachine not run.");
+            return;
+        }
+        if (event.getCurrentItem() == null || event.getCurrentItem().getType() != Material.EXPERIENCE_BOTTLE) {
+            if ("EXPtank" == q) {
+                event.setCancelled(true);
+                if (event.getCurrentItem() != null){
+                    p.sendMessage("入れられるのは経験値瓶のみです！");
+                }
+            }
+        }
+    }
+}
